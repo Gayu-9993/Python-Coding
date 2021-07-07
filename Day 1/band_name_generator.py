93c8d02c5c5aa5d9834aa59b7fed81f01e783721
Gayu-9993 <87092706+Gayu-9993@users.noreply.github.com>
2021-07-08 01:03:58 +0530

    Update band_name_generator.py

--- Day 1/band_name_generator.py
+++ Day 1/band_name_generator.py
@@ -1,1 +1,4 @@
-
+print("Welcome to Band Name Generator!")
+city = input("Name of the city where you grown up:\n")
+name = input("What is your pet name:\n")
+print("Band Name : " + city + " " + name)
